bb14bea5dad62c2586e7aa8c8a72cdecdcc4e194
Maria Eduarda Meregali <melmeregali@gmail.com>
2022-05-30 17:32:07 -0300

    inicialização do projeto

---
+++ books/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from uuid import uuid4 #cria numeração dos IDs
+
+# Create your models here.
+
+class Books(models.Model):
+    id_book = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    Author = models.CharField(max_length=255)
+    release_year = models.IntegerField()
+    state = models.CharField(max_length=50)
+    pages = models.IntegerField()
+    publishing_company = models.CharField(max_length=255)
+    create_at = models.DateField(auto_now_add=True)
+    
